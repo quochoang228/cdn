82a02789381204a95b66293f601ae0302df18ad3
quocht5 <quocht5@viettel.com.vn>
2025-04-18 08:42:51 +0700

    update

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -19,6 +19,13 @@ class $AssetsIconsGen {
   SvgGenImage get iconCircle =>
       const SvgGenImage('assets/icons/Icon Circle.svg');
 
+  /// File path: assets/icons/Profile.svg
+  SvgGenImage get profile => const SvgGenImage('assets/icons/Profile.svg');
+
+  /// File path: assets/icons/briefcase 1.svg
+  SvgGenImage get briefcase1 =>
+      const SvgGenImage('assets/icons/briefcase 1.svg');
+
   /// File path: assets/icons/calendar 1.svg
   SvgGenImage get calendar1 => const SvgGenImage('assets/icons/calendar 1.svg');
 
@@ -37,8 +44,19 @@ class $AssetsIconsGen {
   SvgGenImage get closeCircle =>
       const SvgGenImage('assets/icons/close-circle.svg');
 
+  /// File path: assets/icons/connection.svg
+  SvgGenImage get connection =>
+      const SvgGenImage('assets/icons/connection.svg');
+
+  /// File path: assets/icons/connection_02.svg
+  SvgGenImage get connection02 =>
+      const SvgGenImage('assets/icons/connection_02.svg');
+
   /// File path: assets/icons/copy.svg
   SvgGenImage get copy => const SvgGenImage('assets/icons/copy.svg');
+
+  /// File path: assets/icons/hard-hat 1.svg
+  SvgGenImage get hardHat1 => const SvgGenImage('assets/icons/hard-hat 1.svg');
 
   /// File path: assets/icons/info-circle.svg
   SvgGenImage get infoCircle =>
@@ -47,18 +65,56 @@ class $AssetsIconsGen {
   /// File path: assets/icons/magicpen.svg
   SvgGenImage get magicpen => const SvgGenImage('assets/icons/magicpen.svg');
 
+  /// File path: assets/icons/medal-star.svg
+  SvgGenImage get medalStar => const SvgGenImage('assets/icons/medal-star.svg');
+
+  /// File path: assets/icons/personalcard.svg
+  SvgGenImage get personalcard =>
+      const SvgGenImage('assets/icons/personalcard.svg');
+
+  /// File path: assets/icons/roadmap 1.svg
+  SvgGenImage get roadmap1 => const SvgGenImage('assets/icons/roadmap 1.svg');
+
+  /// File path: assets/icons/social-insurance 1.svg
+  SvgGenImage get socialInsurance1 =>
+      const SvgGenImage('assets/icons/social-insurance 1.svg');
+
+  /// File path: assets/icons/sunbed-holiday-vacation.svg
+  SvgGenImage get sunbedHolidayVacation =>
+      const SvgGenImage('assets/icons/sunbed-holiday-vacation.svg');
+
   /// List of all assets
   List<SvgGenImage> get values => [
     iconCircle,
+    profile,
+    briefcase1,
     calendar1,
     callCalling,
     callCircle,
     clock,
     closeCircle,
+    connection,
+    connection02,
     copy,
+    hardHat1,
     infoCircle,
     magicpen,
+    medalStar,
+    personalcard,
+    roadmap1,
+    socialInsurance1,
+    sunbedHolidayVacation,
   ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/header.png
+  AssetGenImage get header => const AssetGenImage('assets/images/header.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [header];
 }
 
 class CDNAssets {
@@ -67,6 +123,84 @@ class CDNAssets {
   static const String package = 'cdn';
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  static const String package = 'cdn';
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+  }) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/cdn/$_assetName';
 }
 
 class SvgGenImage {
